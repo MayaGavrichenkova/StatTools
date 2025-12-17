3db1ff68b18e778b51573a775b6aa3442c9a007c
Asya Lyanova <64890333+pipipyau@users.noreply.github.com>
2025-12-17 19:08:55 +0300

    feat: anisotropic fBm

--- StatTools/generators/ndfnoise_generator.py
+++ StatTools/generators/ndfnoise_generator.py
@@ -1,7 +1,5 @@
 """Generate n-dimensional fBm field."""
 
-import warnings
-
 import numpy as np
 
 
@@ -31,40 +29,58 @@ def ndfnoise(
         f = ndfnoise((2048, 2048), hurst=0.8)
         ```
     """
-
-    if isinstance(hurst, tuple):
-        warnings.warn("Tuple is not implemented. Use a single float value for hurst.")
-        return
-
     shape = tuple(shape)
     dim = len(shape)
+
+    if isinstance(hurst, (tuple, list, np.ndarray)):
+        if len(hurst) != dim:
+            raise ValueError("hurst must have one value per dimension")
+        hurst = np.asarray(hurst, dtype=dtype)
 
     # The final shape of the rFFT spectrum
     spec_shape = (*shape[:-1], shape[-1] // 2 + 1)
 
-    # Creates an array to store each point of the spectrum
-    k_sq = np.zeros(spec_shape, dtype=dtype)
+    if isinstance(hurst, np.ndarray):  # anisotropic fBm version
+        S = np.ones(spec_shape, dtype=dtype)
+        # Usual FFT axes
+        for i, n in enumerate(shape[:-1]):
+            f = np.fft.fftfreq(n, d=1.0).astype(dtype)
+            reshape = [1] * dim
+            reshape[i] = n
+            reshape[-1] = 1
+            S *= np.abs(f.reshape(reshape)) ** (-(hurst[i] + 0.5))
 
-    # Usual FFT axes
-    for i, n in enumerate(shape[:-1]):
-        f = np.fft.fftfreq(n, d=1.0).astype(dtype)
-        reshape = [1] * dim
-        reshape[i] = n
-        reshape[-1] = 1
-        k_sq += f.reshape(reshape) ** 2
+        # rFFT axis
+        f_last = np.fft.rfftfreq(shape[-1], d=1.0).astype(dtype)
+        S *= np.abs(f_last.reshape((1,) * (dim - 1) + (-1,))) ** (-(hurst[-1] + 0.5))
 
-    # The last rFFT axis
-    f_last = np.fft.rfftfreq(shape[-1], d=1.0).astype(dtype)
-    k_sq += f_last.reshape((1,) * (dim - 1) + (-1,)) ** 2
+        # removing the singularity at zero
+        S[~np.isfinite(S)] = 0.0
 
-    # Radial frequency, argument of the spectrum
-    k = np.sqrt(k_sq, dtype=dtype)
+    else:
+        # Creates an array to store each point of the spectrum
+        k_sq = np.zeros(spec_shape, dtype=dtype)
 
-    # Spectrum initialization
-    alpha = hurst + dim / 2.0
-    S = np.zeros_like(k)
-    nonzero = k > 0
-    S[nonzero] = k[nonzero] ** (-alpha)
+        # Usual FFT axes
+        for i, n in enumerate(shape[:-1]):
+            f = np.fft.fftfreq(n, d=1.0).astype(dtype)
+            reshape = [1] * dim
+            reshape[i] = n
+            reshape[-1] = 1
+            k_sq += f.reshape(reshape) ** 2
+
+        # The last rFFT axis
+        f_last = np.fft.rfftfreq(shape[-1], d=1.0).astype(dtype)
+        k_sq += f_last.reshape((1,) * (dim - 1) + (-1,)) ** 2
+
+        # Radial frequency, argument of the spectrum
+        k = np.sqrt(k_sq, dtype=dtype)
+
+        # Spectrum initialization
+        alpha = hurst + dim / 2.0
+        S = np.zeros_like(k)
+        nonzero = k > 0
+        S[nonzero] = k[nonzero] ** (-alpha)
 
     # Complex white noise
     noise = (
@@ -79,5 +95,4 @@ def ndfnoise(
         std = field.std()
         if std > 0:
             field /= std
-
     return field
